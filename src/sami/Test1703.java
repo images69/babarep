4883ea9d6af572caa28fa34ffd5af90aab3bf04d
sami <sami.torkamani11@yahoo.de>
2017-03-30 10:01:13 +0200

    commit 3 smiley

--- src/sami/Test1703.java
+++ src/sami/Test1703.java
@@ -4,7 +4,7 @@ public class Test1703 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Guten Tag Hr. Sami Torkamani !");
+		System.out.println("Guten Tag Hr. Sami Torkamani :)");
 	}
 
 }
