a692c78228838bfa836cb027a92a55a9053aba00
sami <sami.torkamani11@yahoo.de>
2017-03-30 10:00:26 +0200

    commit 2

--- src/sami/Test1703.java
+++ src/sami/Test1703.java
@@ -5,6 +5,8 @@ public class Test1703 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Guten Tag Hr. Sami Torkamani !");
+		System.out.println("Guten Tag Hr. Sami Torkamani !");
+		System.out.println("Guten Tag Hr. Sami Torkamani !");
 	}
 
 }
